0ddd279137812efbfe8030dfc34407eabac0a20e
yu_gulyaeva <mangostinshop@mail.ru>
2022-11-26 08:36:06 +0300

    Задача 2: нахождение 3 цифры

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (number < 100)
+{
+    Console.WriteLine(" Третьей цифры нет ");
+}
+if (number >= 100)
+{
+    if (number < 1000)
+    result  = number % 100 % 10;
+    
+    else
+    {
+        result = number % 100 / 10;
+    }
+    Console.Write("Третья цифра этого числа ");
+    Console.WriteLine(result);
+}
